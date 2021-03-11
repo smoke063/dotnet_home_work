086f717d142ae81d7754caa70cc4ba06a3ddb438
danila <danila.kononov@netcracker.com>
2021-03-11 14:33:46 +0400

    ++

---
+++ lesson-4/NumbersSeparatedBySpace/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace NumbersSeparatedBySpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine()?.Trim();
+            Console.WriteLine(ConvertStringNumbersToSumNumber(str));
+        }
+
+        static int ConvertStringNumbersToSumNumber(string str = "")
+        {
+            int sum = 0;
+            foreach (var c in str.ToCharArray())
+            {
+                if (!Char.IsWhiteSpace(c))
+                {
+                    int number;
+                    if (int.TryParse(c.ToString(), out number))
+                    {
+                        sum += number;
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ lesson-4/lesson-4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lesson_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Print(GetFullName("Вася", "Васильев", "Александрийский"));
+           Print(GetFullName("Антон", "Хан", "Батыровский"));
+           Print(GetFullName("Леся", "Прохоров", "Махмудовский"));
+        }
+
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            return $@"{firstName} {lastName} {patronymic}";
+        }
+
+        static void Print(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
