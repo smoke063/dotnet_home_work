555020377c092ba5f0d53237eb0480755a31a028
danila <danila.kononov@netcracker.com>
2021-03-14 17:01:51 +0400

    + lesson 5 !

---
+++ lesson-5/BinaryFile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace BinaryFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите набор чисел (0...255):");
+            try
+            {
+                var numbers = Console.ReadLine().Trim().Split(" ").Select(i => int.Parse(i));
+                const string fileName = "Test.dat";
+                using (BinaryWriter writer = new BinaryWriter(File.Open(fileName, FileMode.Create)))
+                {
+                    foreach (var n in numbers)
+                    {
+                        writer.Write(n);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+          
+        }
+    }
+}
---
+++ lesson-5/StartupTxt/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace StartupTxt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "startup.txt";
+            if (!System.IO.File.Exists(file))
+            {
+                File.WriteAllText(file, DateTime.Now.ToString());
+            }
+            else
+            {
+                File.AppendAllText(file, $"{Environment.NewLine}{DateTime.Now.ToString()}");
+
+            }
+        }
+    }
+}
